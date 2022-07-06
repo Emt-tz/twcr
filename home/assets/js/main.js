9ab15ab1b83ca9b3cfce8a6c50b07b6b16833b63
Geeznomous <42958390+Geeznomous@users.noreply.github.com>
2022-07-06 15:24:06 +0300

    new build
    fe

--- home/assets/js/main.js
+++ home/assets/js/main.js
@@ -160,6 +160,7 @@
     loop: true,
     items: 1
   });
+  
 
   // Porfolio isotope and filter
   $(window).on('load', function() {
@@ -189,6 +190,29 @@
     dots: true,
     loop: true,
     items: 1
+  });
+
+  // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center: true,
+    margin: 25,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    }
   });
 
   // Init AOS
